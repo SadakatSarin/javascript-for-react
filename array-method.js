b06dfb4e37098396fc2b124dca3b4b52178b5ae0
unknown <SadakatSarin>
2024-01-10 00:04:51 +0600

    2nd commit

--- array-method.js
+++ array-method.js
@@ -13,3 +13,31 @@ const products = [
 const brand = products.map(products => products.brand);
 
 console.log(brand);
+
+const price = products.map(products => products.price);
+
+console.log(price);
+
+products.forEach(products => console.log(products))
+
+
+products.forEach(products => console.log(products.color))
+
+products.forEach(products => {
+    
+console.log(products.name);
+
+})
+
+// 3. filter
+const cheap = products.filter(products=>products.price < 5000)
+
+console.log(cheap);
+
+const specificName = products.filter(products => products.name.includes('t'));
+
+console.log(specificName);
+
+const special = products.find(p => p.name.includes('n'))
+
+console.log(special);
